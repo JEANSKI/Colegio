7309e2cad8f8732a073ab67bfae54c5cb078ffa1
JEANSKI <85173839+J34NSK1@users.noreply.github.com>
2026-04-13 20:44:02 -0500

    creacion de la superclase persona con metodos get y set

---
+++ Evaluacion_Colegio/src/main/java/Modelo/Persona.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author User
+ */
+public class Persona {
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String fechaNacimiento;
+
+    public Persona() {}
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public String getDireccion() { return direccion; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+    public String getTelefono() { return telefono; }
+    public void setTelefono(String telefono) { this.telefono = telefono; }
+    public String getFechaNacimiento() { return fechaNacimiento; }
+    public void setFechaNacimiento(String fechaNacimiento) { this.fechaNacimiento = fechaNacimiento; }
+
+    @Override
+    public String toString() {
+        return "nombre: " + nombre + " | tel: " + telefono;
+    }
+}
